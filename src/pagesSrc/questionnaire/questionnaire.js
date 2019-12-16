0bb9e42e83b85d84e51b50cad081aa17e7986d47
IamHuadong <chenhuadong94@outlook.com>
2019-12-16 09:34:04 +0800

    删除旧网站

--- src/pagesSrc/questionnaire/questionnaire.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import questionnaire from './questionnaire.vue'
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(questionnaire),
-}).$mount('#questionnaire')
